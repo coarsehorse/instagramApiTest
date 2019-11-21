3fe5986bd78a53a64de7b50edf167424de1430ef
heroys6 <example@example.com>
2019-11-21 21:35:35 +0200

    Fixed session saving logic, now session saved after every request. Fixed relogin problem, when it fired every time.

--- src/main/java/InstagramApiTest.java
+++ src/main/java/InstagramApiTest.java
@@ -7,7 +7,12 @@ import service.CredentialsService;
 import service.SessionDataService;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Created by nani71 on 05/11/2019
@@ -17,68 +22,66 @@ import java.util.*;
 public class InstagramApiTest {
 
     public static void main(String[] args) throws Exception {
-        CredentialsDTO credentials = CredentialsService
-                .readCredentialsFromFile("credentials.json")
-                .orElseThrow(() -> new Exception("Cannot read credentials from file!"));
-
-        Instagram4j instagram4j = login(credentials)
-                .orElseThrow(() -> new Exception("Cannot login"));
+        Instagram4j instagram4j = setup();
 
         // Send direct message
-
-        StatusResult sendMessageResult = instagram4j.sendRequest(InstagramDirectShareRequest
+        StatusResult sendMessageResult = doRequestAndSaveSession(instagram4j, InstagramDirectShareRequest
                 .builder()
                 .shareType(InstagramDirectShareRequest.ShareType.MESSAGE)
-                .recipients(Arrays.asList("4018040809"))
-                .message("Hello insta world again")
-                .build()
-        );
+                .recipients(Collections.singletonList("4018040809"))
+                .message("Hello again, do we meeting today?")
+                .build());
+
+//        if (!Objects.equals(sendMessageResult.getStatus(), "ok")) {
+//            // Try to relogin
+//            instagram4j = login(instagram4j)
+//                    .orElseThrow(() -> new Exception("Cannot login, check [ERROR] logs"));
+//        }
 
         log.info("Message sending result: " + sendMessageResult.getStatus());
 
         // Get comments on the photo
-
-//        InstagramGetMediaCommentsResult getMediaCommentsResult = instagram4j
-//                .sendRequest(new InstagramGetMediaCommentsRequest("2170829562420939502_5873697792", null));
+//        InstagramGetMediaCommentsResult getMediaCommentsResult = doRequestAndSaveSession(instagram4j,
+//                new InstagramGetMediaCommentsRequest("2170829562420939502_5873697792", null));
 //
 //        log.info("Media comments result: " + getMediaCommentsResult.getComments());
 
         // Get pending
-
 //        InstagramPendingInboxResult instagramPendingInboxResult =
-//                instagram4j.sendRequest(new InstagramGetPendingInboxRequest(null));
+//                doRequestAndSaveSession(instagram4j, new InstagramGetPendingInboxRequest(null));
 //
 //        log.info("" + instagramPendingInboxResult.getInbox());
 
         // Get inbox
-
 //        InstagramInboxResult instagramInboxResult =
-//                instagram4j.sendRequest(new InstagramGetInboxRequest(null));
+//                doRequestAndSaveSession(instagram4j, new InstagramGetInboxRequest(null));
 //
-//        log.info("instagramInboxResult: " /*+ instagramInboxResult.getInbox()*/);
-//
-        // Get pending
+//        log.info("instagramInboxResult :: unseen_count = {}", instagramInboxResult.getInbox().getUnseen_count());
 
-//        InstagramPendingInboxResult instagramPendingInboxResult =
-//                instagram4j.sendRequest(new InstagramGetPendingInboxRequest(instagramInboxResult.getInbox().getThreads().get(0).getNewest_cursor()));
+        // Get pending
+//        InstagramPendingInboxResult instagramPendingInboxResult1 =
+//                doRequestAndSaveSession(instagram4j, new InstagramGetPendingInboxRequest(
+//                        instagramInboxResult.getInbox().getThreads().get(0).getNewest_cursor()));
 //
-//        log.info("Newest cursor: " + instagramPendingInboxResult.getInbox());
+//        log.info("Newest cursor: " + instagramPendingInboxResult1.getInbox());
 
         // Upload photo
-//        InstagramConfigurePhotoResult instagramConfigurePhotoResult =
-//                instagram4j.sendRequest(new InstagramUploadPhotoRequest(
-//                    new File("C:/Users/User/Desktop/insta_photo.jpg"),
-//                    "How am I doing TT?"));
+//        InstagramConfigurePhotoResult instagramConfigurePhotoResult = doRequestAndSaveSession(
+//                instagram4j, new InstagramUploadPhotoRequest(
+//                        new File("C:/Users/User/Desktop/China.jpg"),
+//                        "Best joke - well forgotten old joke"));
 //
 //        log.info("Photo: " + instagramConfigurePhotoResult.toString());
     }
 
     /**
-     * Login with handling instagram challenge(confirmation via sms).
-     *
-     * @return Optional with Instagram API object on success, empty Optional otherwise.
+     * Setup Instagra4j object with proper credentials and last session data.
      */
-    private static Optional<Instagram4j> login(CredentialsDTO credentials) {
+    private static Instagram4j setup() throws Exception {
+        CredentialsDTO credentials = CredentialsService
+                .readCredentialsFromFile("credentials.json")
+                .orElseThrow(() -> new Exception("Cannot read credentials from file"));
+
         Instagram4j instagram4j = SessionDataService
                 .loadSessionData(credentials.getLogin())
                 .map(sessionDataDTO -> Instagram4j
@@ -89,7 +92,7 @@ public class InstagramApiTest {
                         .password(credentials.getPassword())
                         .build()
                 )
-                .orElse(Instagram4j
+                .orElseGet(() -> Instagram4j
                         .builder()
                         .username(credentials.getLogin())
                         .password(credentials.getPassword())
@@ -98,71 +101,96 @@ public class InstagramApiTest {
 
         instagram4j.setup();
 
-        try {
-            InstagramLoginResult instagramLoginResult = instagram4j.login();
+        return instagram4j;
+    }
 
-            if (Objects.equals(instagramLoginResult.getStatus(), "ok")) {
-                log.info("Logged in as usual");
-                SessionDataService.saveSessionData(
-                        credentials.getLogin(), instagram4j.getUuid(), instagram4j.getCookieStore()
-                );
-                return Optional.of(instagram4j);
+    /**
+     * Login with handling instagram challenge.
+     *
+     * @return Optional with Instagram API object on success, empty Optional otherwise.
+     */
+    private static Optional<Instagram4j> login(Instagram4j instagram4j) throws IOException {
+        InstagramLoginResult instagramLoginResult = instagram4j.login();
+
+        if (Objects.equals(instagramLoginResult.getStatus(), "ok")) {
+            log.info("Logged in as usual");
+            SessionDataService.saveSessionData(
+                    instagram4j.getUsername(), instagram4j.getUuid(), instagram4j.getCookieStore()
+            );
+            return Optional.of(instagram4j);
+        }
+        else if (Objects.equals(instagramLoginResult.getError_type(), "checkpoint_challenge_required")) {
+            // Challenge required
+
+            // Get challenge URL
+            String challengeUrl = instagramLoginResult.getChallenge().getApi_path().substring(1);
+
+            // Reset challenge
+            String resetChallengeUrl = challengeUrl
+                    .replace("challenge", "challenge/reset");
+            InstagramGetChallengeResult getChallengeResult =
+                    instagram4j.sendRequest(new InstagramResetChallengeRequest(resetChallengeUrl));
+
+            // If action is close
+            if (Objects.equals(getChallengeResult.getAction(), "close")) {
+                // Get challenge
+                getChallengeResult = instagram4j
+                        .sendRequest(new InstagramGetChallengeRequest(challengeUrl));
             }
-            else if (Objects.equals(instagramLoginResult.getError_type(), "checkpoint_challenge_required")) {
-                // Challenge required
+            else if (Objects.equals(getChallengeResult.getStep_name(), "select_verify_method")) {
+                // Get security code
+                InstagramSelectVerifyMethodResult postChallengeResult = instagram4j
+                        .sendRequest(new InstagramSelectVerifyMethodRequest(challengeUrl,
+                                getChallengeResult.getStep_data().getChoice()));
 
-                // Get challenge URL
-                String challengeUrl = instagramLoginResult.getChallenge().getApi_path().substring(1);
-
-                // Reset challenge
-                String resetChallengeUrl = challengeUrl
-                        .replace("challenge", "challenge/reset");
-                InstagramGetChallengeResult getChallengeResult =
-                        instagram4j.sendRequest(new InstagramResetChallengeRequest(resetChallengeUrl));
-
-                // If action is close
-                if (Objects.equals(getChallengeResult.getAction(), "close")) {
-                    // Get challenge
-                    getChallengeResult = instagram4j
-                            .sendRequest(new InstagramGetChallengeRequest(challengeUrl));
+                System.out.print("Please, input the security code from SMS/email (XXX XXX) without spaces: ");
+                String securityCode = null;
+                try (Scanner scanner = new Scanner(System.in)) {
+                    securityCode = scanner.nextLine();
                 }
 
-                if (Objects.equals(getChallengeResult.getStep_name(), "select_verify_method")) {
-                    // Get security code
-                    InstagramSelectVerifyMethodResult postChallengeResult = instagram4j
-                            .sendRequest(new InstagramSelectVerifyMethodRequest(challengeUrl,
-                                    getChallengeResult.getStep_data().getChoice()));
+                // Send security code
+                InstagramLoginResult securityCodeInstagramLoginResult = instagram4j
+                        .sendRequest(new InstagramSendSecurityCodeRequest(challengeUrl, securityCode));
 
-                    System.out.print("Please, input the security code from SMS (XXX XXX) without spaces: ");
-
-                    String securityCode = null;
-                    try (Scanner scanner = new Scanner(System.in)) {
-                        securityCode = scanner.nextLine();
-                    }
-
-                    // Send security code
-                    InstagramLoginResult securityCodeInstagramLoginResult = instagram4j
-                            .sendRequest(new InstagramSendSecurityCodeRequest(challengeUrl, securityCode));
-
-                    if (Objects.equals(securityCodeInstagramLoginResult.getStatus(), "ok")) {
-                        log.info("Logged in successfully after passing the challenge");
-                        SessionDataService.saveSessionData(
-                                credentials.getLogin(), instagram4j.getUuid(), instagram4j.getCookieStore()
-                        );
-                        return Optional.of(instagram4j);
-                    } else {
-                        log.error("Cannot login even after passing the challenge");
-                        return Optional.empty();
-                    }
+                if (Objects.equals(securityCodeInstagramLoginResult.getStatus(), "ok")) {
+                    log.info("Logged in successfully after passing the challenge");
+                    SessionDataService.saveSessionData(
+                            instagram4j.getUsername(), instagram4j.getUuid(), instagram4j.getCookieStore()
+                    );
+                    return Optional.of(instagram4j);
+                } else {
+                    log.error("Cannot login even after passing the challenge");
+                    return Optional.empty();
                 }
             } else {
-                log.error("Cannot login as usual + checkpoint challenge is not required");
+                log.error("checkpoint_challenge_required " +
+                        " - unhandled case, getChallengeResult: {}", getChallengeResult);
                 return Optional.empty();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            log.error("Cannot login as usual + checkpoint challenge is not required");
+            return Optional.empty();
         }
 
+        log.error("Cannot login, unhandled case, instagramLoginResult: {}", instagramLoginResult);
         return Optional.empty();
+    }
+
+    /**
+     * Do request and save changed session data.
+     *
+     * @param instagram4j Main API object that has passed setup.
+     * @param request     Instagram request.
+     * @return result of request.
+     */
+    public static <T extends StatusResult, R extends InstagramRequest<T>> T doRequestAndSaveSession(
+            Instagram4j instagram4j, R request) throws IOException {
+        T statusResult = instagram4j.sendRequest(request);
+
+        SessionDataService.saveSessionData(
+                instagram4j.getUsername(), instagram4j.getUuid(), instagram4j.getCookieStore());
+
+        return statusResult;
     }
 }
